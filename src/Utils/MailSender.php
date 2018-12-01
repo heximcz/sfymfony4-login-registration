79acc161c734377c4529dae66ebc426fbc17ea91
Lubomir Spacek <admin@best-net.cz>
2018-12-01 22:17:35 +0100

    fix Translation deprecated warning

--- src/Utils/MailSender.php
+++ src/Utils/MailSender.php
@@ -4,7 +4,7 @@ namespace App\Utils;
 
 use App\Entity\User;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MailSender
 {
